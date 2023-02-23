f0716bd91b2132b2f0fb48b2b0f3aa003513918e
David Thompson <davidbthomp@gmail.com>
2023-02-23 14:03:03 -0600

    part1c

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,37 +1,44 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
-const Footer = () => {
-  return (
-    <div>
-      Learning react with <a href='https://github.com/DavidBThomp/fullStackOpen'>DevBThom</a>
-    </div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
-  const friends = [{name: 'Aman', age: 22}, {name: 'James', age: 23}]
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
-  <>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-
-      <Footer />
-    </>
+    <div>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />   
+    </div>
   )
 }
 
