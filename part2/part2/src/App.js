51f954c60a3d7d69e095a407242efa86aa572c9d
David Thompson <davidbthomp@gmail.com>
2023-03-07 14:06:40 -0600

    extracting communication into module

--- part2/part2/src/App.js
+++ part2/part2/src/App.js
@@ -1,47 +1,53 @@
 import { useState, useEffect } from 'react'
 
-import axios from 'axios'
-
+import noteService from './services/notes'
 import Note from './components/Note'
 
 const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        setNotes(response.data)
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
-  }
-  
-  // Run function hook, on first refresh
-  useEffect(hook, [])
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+      important: Math.random() > 0.5,
     }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+      })
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
   const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important === true)
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
@@ -50,19 +56,18 @@ const App = () => {
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
-      </div>
+      </div> 
+      <ul>
         <ul>
-        {notesToShow.map(note =>
-            <Note key={note.id} note={note} />
+          {notesToShow.map(note => 
+            <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
           )}
         </ul>
-        <form onSubmit={addNote}>
-        <input 
-        value={newNote}
-        onChange={handleNoteChange}
-        />
-          <button type="submit">save</button>
-        </form>   
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
--- part2/part2/src/index.js
+++ part2/part2/src/index.js
@@ -1,29 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
 import App from './App'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
-
-// Handled in db.json under notes object
-
-// const notes = [
-//   {
-//     id: 1,
-//     content: 'HTML is easy',
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: 'Browser can execute only JavaScript',
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: 'GET and POST are the most important methods of HTTP protocol',
-//     important: true
-//   }
-// ]
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
