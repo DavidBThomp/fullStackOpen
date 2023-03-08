4ce66ba65015494daed2ff0b7d8721815a713f9c
David Thompson <davidbthomp@gmail.com>
2023-03-08 11:54:40 -0600

    exercise2.12

--- part2/exercise2.6/src/App.js
+++ part2/exercise2.6/src/App.js
@@ -37,6 +37,12 @@ const App = () => {
       phone: newPhone
     }
 
+    axios
+    .post('http://localhost:3001/persons', nameObject)
+    .then( response => {
+      console.log(response)
+    })
+
     setPersons(persons.concat(nameObject))
     setNewName('')
     setNewPhone('')
