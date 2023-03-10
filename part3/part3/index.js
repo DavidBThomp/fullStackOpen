3b0625b979fead6054aa53bab3ad5a3db7edcab2
Brendan Thompson <65555644+DavidBThomp@users.noreply.github.com>
2023-03-10 16:30:12 -0600

    exercise3.1

--- part3/part3/index.js
+++ part3/part3/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const app = express()
 
+// Built in JSON parser
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -19,6 +22,9 @@ let notes = [
     important: true
   }
 ]
+
+const generateId = () => Math.max(...notes.map(n => n.id))
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
@@ -27,6 +33,45 @@ app.get('/', (request, response) => {
     response.json(notes)
   })
   
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })  
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
+
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId(),
+    }
+  
+    // Doesn't add to array because concat doesn't mutate data
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
