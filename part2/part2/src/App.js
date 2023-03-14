02ba779437d029f18cbeefbd3cfd6b4f3b00f090
David Thompson <davidbthomp@gmail.com>
2023-03-14 15:56:21 -0500

    part3b

--- part2/part2/src/App.js
+++ part2/part2/src/App.js
@@ -76,7 +76,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes App</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
