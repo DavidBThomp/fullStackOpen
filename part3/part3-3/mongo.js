0f8af18e259b0d59e607fa41ac40a1836f3d7f31
David Thompson <davidbthomp@gmail.com>
2023-03-17 13:54:56 -0500

    part3c start

---
+++ part3/part3-3/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = encodeURIComponent(process.argv[2])
+
+const url =
+  `mongodb+srv://davidbthomp:${password}@cluster0.yq92yht.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// 
+// Finding notes with query
+// 
+
+Note.find({ important: false }).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+})
+
+// 
+// Adding Notes
+// 
+
+// const note = new Note({
+//   content: 'Database:noteApp, Collection: Notes, Document: This object',
+//   important: false,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
